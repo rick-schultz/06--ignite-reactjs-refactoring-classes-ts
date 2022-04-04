7e077a31fe3ae3c428dbb59c8d70c5fabe6eed61
Rick Schultz <rick-schultz@hotmail.com>
2022-04-04 12:09:54 -0300

    resolve styles and main components

--- src/components/Food/styles.ts
+++ src/components/Food/styles.ts
@@ -1,6 +1,10 @@
 import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface InputProps {
+  available: boolean;
+}
+
+export const Container = styled.div<InputProps>`
   background: #f0f0f5;
   border-radius: 8px;
 
