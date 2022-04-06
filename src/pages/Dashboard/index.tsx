e09c84cb2870f239bf6670752742c3257ece933b
Rick Schultz <rick-schultz@hotmail.com>
2022-04-05 23:21:40 -0300

    Resolve Dashboard Page :tada:

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,18 +3,9 @@ import { useState, useEffect } from 'react'
 import Header from '../../components/Header'
 import api from '../../services/api'
 import Food from '../../components/Food'
-import ModalAddFood from '../../components/ModalAddFood'
+import ModalAddFood, { FoodItemProps } from '../../components/ModalAddFood'
 import ModalEditFood from '../../components/ModalEditFood'
 import { FoodsContainer } from './styles'
-
-interface FoodItemProps {
-  id: number
-  name: string
-  description: string
-  price: number
-  available: boolean
-  image_url?: string
-}
 
 function Dashboard() {
   const [foods, setFoods] = useState<FoodItemProps[]>([])
